a2a3c88c992be73031d838a8469f269726cdfb3d
Rami Hadad <rami@ramihikmat.net>
2021-01-26 14:35:58 +1100

    Added Vue.js for UI

---
+++ ui/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="app" class="container mt-5">
+		<form @click.prevent="formSubmit()">
+			<div class="mb-3">
+				<label for="company" class="form-label">Company name: </label>
+				<input type="text" class="form-control" id="company" :value="company">
+			</div>
+			<div class="mb-3">
+				<label for="title" class="form-label">Job title: </label>
+				<input type="text" class="form-control" id="title" :value="title">
+			</div>
+			<div class="mb-3">
+				<label for="link" class="form-label">Url: </label>
+				<input type="text" class="form-control" id="link" :value="link">
+			</div>
+			<button type="submit" class="btn btn-primary">Add</button>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+	  return {
+		  company: "",
+		  title: "",
+		  link: ""
+	  };
+  },
+  methods: {
+	formSubmit(){
+		console.log("Hello world");
+	}
+  }
+}
+</script>
